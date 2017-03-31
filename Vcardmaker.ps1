1cefbabff550b01d4f32caee4250c3c9e0c1b785
AJEversdijk <AJEversdijk@gmail.com>
2017-03-31 14:39:02 +0200

    Initial
    I needed to make an apple contact info file, and doing this for a lot of contacts is tedious.

---
+++ Vcardmaker.ps1
@@ -0,0 +1,14 @@
+﻿##VCard maker
+BEGIN:VCARD
+VERSION:3.0
+PRODID:-//Apple Inc.//iOS 10.2//EN
+N:<lastname>;<firstname>;;;
+FN:<firstname> <lastname>
+ORG:<organization>;
+TITLE:<title>
+item1.EMAIL;type=INTERNET;type=pref:<email>
+item1.X-ABLabel:_$!<Other>!$_
+TEL;type=WORK;type=VOICE;type=pref:<Phone>
+REV:2017-01-20T14:21:05Z
+END:VCARD
+
