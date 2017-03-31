e5b930d0b547d0e52cdcdcc84062be7b428fc076
AJEversdijk <AJEversdijk@gmail.com>
2017-03-31 15:09:17 +0200

    initial

--- DesktopAutoFiler.ps1
+++ DesktopAutoFiler.ps1
@@ -1,9 +1,9 @@
 ﻿##Designed to quickly file items on the desktop into the propper folders
 ##Close the items you want to move before running this
-
+##Made more as a training excercise than something actually usefull
 
 $station = "C"
-$user = "Anne Jan Eversdijk"
+$user = "<user>"
 $desktopLocation = -join($station, ":\Users\", $user, "\Desktop")
 $filingdirectoryImages = "Pictures"
 $filingDirectoryVideos = "Videos"
@@ -35,7 +35,7 @@ Function Get-ChildItemNamesForLocation
 
     Try
     {
-        $childItems = Get-ChildItem -Path "C:\Users\Anne Jan Eversdijk\Desktop"
+        $childItems = Get-ChildItem -Path (-join($station, ":\Users\", $user, "\Desktop"))
         
     }
     Catch
@@ -56,15 +56,16 @@ Function Sort-DesktopItem
             Write-Host $childItem.Name
             switch ($childItem.Name)
             {
-                #should eventually store this info in a sepparate file so "bmp, Images" woulg go in that file, 
+                #should eventually store this info in a sepparate file so "bmp, Images" would go in that file, 
                 #it would be easier to add a long list to a file like that.
                 "*.jpg" {Move-DesktopItemToSetDirectory $childItem $filingdirectoryImages}
                 "*.bmp" {Move-DesktopItemToSetDirectory $childItem $filingdirectoryImages}
-                "*.txt" {}
-                "*.doc" {}
-                "*.exe" {}
-                "*.mp4" {}
-                "*.mkv" {}
+                "*.txt" {Move-DesktopItemToSetDirectory $childItem$filingDirectoryTekstDocuments}
+                "*.doc" {Move-DesktopItemToSetDirectory $childItem$filingDirectoryTekstDocuments}
+                "*.exe" {Move-DesktopItemToSetDirectory $childItem$filingDirectoryPrograms}
+                "*.mp4" {Move-DesktopItemToSetDirectory $childItem$filingDirectoryVideos}
+                "*.mkv" {Move-DesktopItemToSetDirectory $childItem$filingDirectoryVideos}
+                "*.ps1" {Move-DesktopItemToSetDirectory $childItem$filingDirectoryCodeSnipets}
             }
         }
     }
@@ -81,13 +82,15 @@ Function Move-DesktopItemToSetDirectory
 
     Try
     {
-        If(!Test-path(-join($location, "\", $fileItem.Name)))
+        $destinationFilename = -join($location, "\", $fileItem.Name)
+        If(!Test-path($destinationFilename))
         {
-            Move-Item -path $fileItem.Path -Destination $location
+            Move-Item -path $fileItem.Path -Destination $destinationFilename
         }
         Else
         {
-            Move-Item -path $fileItem.Path -Destination $location
+            Move-Item -path $fileItem.Path -Destination (-join($destinationFilename, "1"))
+
         }
     }
     Catch
@@ -104,7 +107,7 @@ Function Create-DirectoryIfNotExists
         
         $foldersToTestOrCreate = New-Object system.Collections.ArrayList
         #you need to create the entire folder structure here, I havn't bothered with an external conig for this
-        $foldersToTestOrCreate.Add((-join($station,":\Users\", $user, "\", $filingdirectoryImages)))
+        $foldersToTestOrCreate.Add((-join($station, ":\Users\", $user, "\", $filingdirectoryImages)))
         $foldersToTestOrCreate.Add((-join($station, ":\Users\", $user, "\", $filingDirectoryVideos)))
         $foldersToTestOrCreate.Add((-join($station, ":\Users\", $user, "\", $filingDirectoryTekstDocuments)))
         $foldersToTestOrCreate.Add((-join($station, ":\Users\", $user, "\", $filingDirectoryCodeSnipets)))
